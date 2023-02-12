bc23311eb9e0e442a248417755231a5dacbc9204
Stéphane Lasmartres <slasmartres@gmail.com>
2023-02-12 16:19:06 +0100

    Added more test cases

--- reverse-string/csharp/ReverseStringSpec.cs
+++ reverse-string/csharp/ReverseStringSpec.cs
@@ -8,6 +8,8 @@ public class ReverseStringSpec
 {
     [Theory]
     [InlineData("Cat", "taC")]
+    [InlineData("The Daily Byte", "etyB yliaD ehT")]
+    [InlineData("civic", "civic")]
     public void ShouldReverseString(string input, string expected)
     {
         reverse(input).Should().Be(expected);
