b02c0200e33d2a082e2c11dd236f484d0ae66e2c
Stéphane Lasmartres <slasmartres@gmail.com>
2023-02-13 21:39:02 +0100

    Init test project

---
+++ valid-palindrome/csharp/ValidPalindrome.Tests/ValidPalindromeSpec.cs
@@ -0,0 +1,13 @@
+namespace ValidPalindrome.Tests;
+
+public class ValidPalindromeSpec
+{
+    //"level", return true
+    //"algorithm", return false
+    //"A man, a plan, a canal: Panama.", return true
+
+    [Fact]
+    public void Test1()
+    {
+    }
+}
