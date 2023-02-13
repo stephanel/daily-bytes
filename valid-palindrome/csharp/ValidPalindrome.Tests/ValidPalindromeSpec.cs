e89e56bdcbda5a6642821fdaf0d485fdc6954790
Stéphane Lasmartres <slasmartres@gmail.com>
2023-02-13 22:18:28 +0100

    Added implementation using ascii code and for loop

--- valid-palindrome/csharp/ValidPalindrome.Tests/ValidPalindromeSpec.cs
+++ valid-palindrome/csharp/ValidPalindrome.Tests/ValidPalindromeSpec.cs
@@ -10,22 +10,48 @@ public class ValidPalindromeSpec
     [InlineData("algorithm", false)]
     [InlineData("A man, a plan, a canal: Panama.", true)]
     [InlineData("ABCD;!*$dcba.", true)]
-    public void ShouldValidatePalindrome(string input, bool expected)
+    public void ShouldValidatePalindromeAsciiCodeLinq(string input, bool expected)
     {
-        bool actual = validate(input);
-        actual.Should().Be(expected);
+        ValidateUsingAsciiCodeLinq(input).Should().Be(expected);
     }
 
-    private bool validate(string v)
+    [Theory]
+    [InlineData("level", true)]
+    [InlineData("algorithm", false)]
+    [InlineData("A man, a plan, a canal: Panama.", true)]
+    [InlineData("ABCD;!*$dcba.", true)]
+    public void ShouldValidatePalindromeAsciiCodeForLoop(string input, bool expected)
+    {
+        ValidateUsingAsciiCodeForLoop(input).Should().Be(expected);
+    }
+    
+    private bool ValidateUsingAsciiCodeLinq(string v)
     {
         var cleaned = v
-            .Replace(" ", string.Empty)
-            .Replace(",", string.Empty)
-            .Replace(":", string.Empty)
-            .Replace(".", string.Empty)
             .ToLower()
+            .Where(c => (short)c >= 97 && (short)c <= 122)
             ;
-        var reverted = string.Join(string.Empty, cleaned.Reverse());
-        return cleaned == reverted;
+
+        var reverted = cleaned.Reverse();
+        return Enumerable.SequenceEqual(cleaned, reverted);
+    }
+
+    private bool ValidateUsingAsciiCodeForLoop(string v)
+    {
+        var cleaned = v
+            .ToLower()
+            .Where(c => (short)c >= 97 && (short)c <= 122)
+            .ToArray();
+
+        var midLength = (cleaned.Length - 1) / 2;
+        for (int i = 0; i <= midLength; i++)
+        {
+            var j = cleaned.Length - 1 - i;
+            if (cleaned[i] != cleaned[j])
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
