a60e70cfebb89c5353661bc15595322f037c351c
Stéphane Lasmartres <slasmartres@gmail.com>
2023-02-20 15:36:39 +0100

    refactored

--- vaccum-cleaner-route/csharp/VaccumCleanerRoute.Tests/VaccumCleanerRouteSpec.cs
+++ vaccum-cleaner-route/csharp/VaccumCleanerRoute.Tests/VaccumCleanerRouteSpec.cs
@@ -7,21 +7,37 @@ public class VaccumCleanerRouteSpec
     //"RUULLDRD", return true
 
     [Fact]
-    public void ShouldReturnTrue_WhenCommandIsLR()
+    public void ShouldReturnTrue_WhenCommandsAreLR()
     {
         var actual = when("LR");
         actual.Should().BeTrue();
     }
 
     [Fact]
-    public void ShouldReturnTrue_WhenCommandIsURURD()
+    public void ShouldReturnTrue_WhenCommandsAreURURD()
     {
         var actual = when("URURD");
         actual.Should().BeFalse();
     }
 
-    private bool when(string command)
+    private bool when(string commands)
     {
-        return true;
+        var numL = countLetter(commands, 'L');
+        var numR = countLetter(commands, 'R');
+        var numU = countLetter(commands, 'U');
+        var numD = countLetter(commands, 'D');
+
+        if (numL == numR && numU == numD)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private int countLetter(string commands, char search)
+    {
+        // what is the best way to count the number of an occurence in a string?
+        // for now, let's do it using LINQ
+        return commands.Count(c => c.Equals(search));
     }
 }
