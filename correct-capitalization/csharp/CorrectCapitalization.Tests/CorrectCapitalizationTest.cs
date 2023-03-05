20908127962ebf0ac2464a4188007be540bfb504
Stéphane Lasmartres <slasmartres@gmail.com>
2023-03-05 19:29:00 +0100

    added test for 'only the first letter is capitalized'

--- correct-capitalization/csharp/CorrectCapitalization.Tests/CorrectCapitalizationTest.cs
+++ correct-capitalization/csharp/CorrectCapitalization.Tests/CorrectCapitalizationTest.cs
@@ -10,11 +10,14 @@ public class CorrectCapitalizationTest
     [Theory]
     [InlineData("USA")]
     [InlineData("NATO")]
+    [InlineData("Calvin")]
     public void ShouldHaveCorrectCapitalization(string input)
         => IsCapitalizationCorrect(input).Should().BeTrue();
 
     private bool IsCapitalizationCorrect(string v)
     {
-        return v.Where(c => char.IsUpper(c)).Count() == v.Length;
+        return v.Where(c => char.IsUpper(c)).Count() == v.Length
+            || (char.IsUpper(v.First())
+                && v.Skip(1).Where(c => char.IsLower(c)).Count() == v.Length-1);
     }
 }
