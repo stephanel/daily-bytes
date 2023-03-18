e56696ac00feb2ba5a626cc1ed66eafe0d429529
Stéphane Lasmartres <slasmartres@gmail.com>
2023-03-18 17:16:27 +0100

    Added dotnet projects for frontend and backend

---
+++ opentelemetry/csharp/microservice/frontend/WeatherClient.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+
+namespace frontend
+{
+    public class WeatherClient
+    {
+        private readonly JsonSerializerOptions options = new JsonSerializerOptions()
+        {
+            PropertyNameCaseInsensitive = true,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        };
+
+        private readonly HttpClient client;
+
+        public WeatherClient(HttpClient client)
+        {
+            this.client = client;
+        }
+
+        public async Task<WeatherForecast[]> GetWeatherAsync()
+        {
+            return await this.client.GetFromJsonAsync<WeatherForecast[]>("/weatherforecast");
+        }
+    }
+}
