36dac4b406e9ed52146ec5780c83efd30cae4fcd
Stephane Lasmartres <stephane.lasmartres@maersk.com>
2024-04-23 11:42:32 +0200

    tested chain of responsibility

---
+++ patterns/chain-of-responsibility/csharp/CSharp.ChainOfResponsibility.Tests/Handlers/OrderProcessHandler.cs
@@ -0,0 +1,19 @@
+﻿using CSharp.ChainOfResponsibility.Tests.Messages;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CSharp.ChainOfResponsibility.Tests.Handlers;
+
+internal class OrderProcessHandler
+{
+    private readonly IHandler _orderValidation;
+
+    public OrderProcessHandler([FromKeyedServices(nameof(OrderValidation))] IHandler orderValidation)
+    {
+        _orderValidation = orderValidation;
+    }
+
+    public void Handle(OrderReceivedMessage orderReceived)
+    {
+        _orderValidation.Handle(orderReceived);
+    }
+}
