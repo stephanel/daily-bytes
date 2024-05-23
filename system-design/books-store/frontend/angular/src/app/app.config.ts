9448092c5d845fdd5d5f0a033618abf16ad6b6ab
Stephane Lasmartres <stephane.lasmartres@maersk.com>
2024-05-23 18:11:40 +0200

    added global error handler

--- system-design/books-store/frontend/angular/src/app/app.config.ts
+++ system-design/books-store/frontend/angular/src/app/app.config.ts
@@ -1,11 +1,13 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, ErrorHandler, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { HttpClientModule } from '@angular/common/http';
+import { GlobalErrorHandler } from './interceptors/error-handler.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(appRoutes),
-    importProvidersFrom(HttpClientModule)
+    importProvidersFrom(HttpClientModule),
+    { provide: ErrorHandler, useClass: GlobalErrorHandler }
   ]
 };
---
+++ system-design/books-store/frontend/angular/src/app/interceptors/error-handler.interceptor.ts
@@ -0,0 +1,11 @@
+import { ErrorHandler, Injectable } from "@angular/core";
+
+
+@Injectable()
+export class GlobalErrorHandler implements ErrorHandler {
+
+  handleError(error: any): void {
+    console.error('An error occurred!', error);
+  }
+
+}
