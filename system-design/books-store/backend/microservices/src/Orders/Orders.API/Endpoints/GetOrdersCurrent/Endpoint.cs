4cb1bcec9355abe9bb2f31473ed2d8cb17725b24
Stephane Lasmartres <stephane.lasmartres@maersk.com>
2024-06-07 20:58:41 +0200

    added orders endpoints

---
+++ system-design/books-store/backend/microservices/src/Orders/Orders.API/Endpoints/GetOrdersCurrent/Endpoint.cs
@@ -0,0 +1,17 @@
+﻿using FastEndpoints;
+
+namespace Orders.API.Endpoints.GetOrdersCurrent;
+
+internal class Endpoint : EndpointWithoutRequest
+{
+    public override void Configure()
+    {
+        Get("api/orders/current");
+        AllowAnonymous();
+    }
+
+    public override Task HandleAsync(CancellationToken ct)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ system-design/books-store/backend/microservices/src/Orders/Orders.API/Endpoints/PostOrdersAddItem/Endpoint.cs
@@ -0,0 +1,20 @@
+﻿using FastEndpoints;
+using Mediator;
+
+namespace Orders.API.Endpoints.PostOrdersAddItem;
+
+internal sealed class Endpoint : Endpoint<AddItemRequest>
+{
+    public override void Configure()
+    {
+        Post("api/orders/items/{itemId}");
+        AllowAnonymous();
+    }
+
+    public override Task HandleAsync(AddItemRequest request, CancellationToken ct)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+internal record AddItemRequest(long ItemId);
