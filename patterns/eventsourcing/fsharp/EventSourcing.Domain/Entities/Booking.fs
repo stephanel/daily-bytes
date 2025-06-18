e051f88f3e70d36b899cae9905f284c87725902b
Stephane Lasmartres <stlas@eg.fi>
2025-06-18 16:11:34 +0300

    added booked time slot changed

--- patterns/eventsourcing/fsharp/EventSourcing.Domain/Entities/Booking.fs
+++ patterns/eventsourcing/fsharp/EventSourcing.Domain/Entities/Booking.fs
@@ -1,15 +1,13 @@
 module EventSourcing.Domain.Entities.Booking
 
-open System.Collections.Generic
 open System
 
 open EventSourcing.Domain.Enums.MeetingRoom
 
-type Attendee = {
-    FirstName: string
-    LastName: string
-    Email: string
-}
+type Attendee =
+    { FirstName: string
+      LastName: string
+      Email: string }
 
 // type BookingCreated = {
 //     Id: Guid
@@ -20,20 +18,44 @@ type Attendee = {
 // }
 
 type Event =
-    | BookingCreated of id : Guid * meetingRoom: MeetingRoom * startTime: DateTime * endTime: DateTime * attendees: IReadOnlyList<Attendee>
+    | BookingCreated of
+        id: Guid *
+        meetingRoom: MeetingRoom *
+        startTime: DateTime *
+        endTime: DateTime *
+        attendees: ResizeArray<Attendee>
+    | BookedTimeSlotChanged of startTime: DateTime * endTime: DateTime
 
-type Booking(id0: Guid, meetingRoom0: MeetingRoom, startTime0: DateTime, endTime0: DateTime, attendees0: IReadOnlyList<Attendee>) =
+type Booking
+    (id0: Guid, meetingRoom0: MeetingRoom, startTime0: DateTime, endTime0: DateTime, attendees0: ResizeArray<Attendee>)
+    =
     let mutable id = id0
     let mutable meetingRoom = meetingRoom0
     let mutable startTime = startTime0
     let mutable endTime = endTime0
     let mutable attendees = attendees0
-    let mutable events : IReadOnlyList<Event> = []
-    do
-        events <- [BookingCreated( id = id, meetingRoom = meetingRoom, startTime = startTime, endTime = endTime, attendees = attendees)]
-    member this.MeetingRoom with get() = meetingRoom
-    member this.StartTime with get() = startTime
-    member this.EndTime with get() = endTime
-    member this.Attendees with get() = attendees
-    member this.Events with get() = events
+    let mutable events = ResizeArray<Event>()
 
+    do
+        events <-
+            ResizeArray<Event>(
+                [ BookingCreated(
+                      id = id,
+                      meetingRoom = meetingRoom,
+                      startTime = startTime,
+                      endTime = endTime,
+                      attendees = attendees
+                  ) ]
+            )
+
+    member this.Id = id
+    member this.MeetingRoom = meetingRoom
+    member this.StartTime = startTime
+    member this.EndTime = endTime
+    member this.Attendees = attendees
+    member this.Events = events
+
+    member this.UpdateTimeSlot(newStartTime: DateTime, newEndTime: DateTime) =
+        startTime <- newStartTime
+        endTime <- newEndTime
+        events.Add(BookedTimeSlotChanged(startTime = startTime, endTime = endTime))
--- patterns/eventsourcing/fsharp/EventSourcing.UnitTests/BookingTests.fs
+++ patterns/eventsourcing/fsharp/EventSourcing.UnitTests/BookingTests.fs
@@ -24,7 +24,7 @@ let ``Create new booking appends BookingCreated event`` () =
     let room = MeetingRoom.Helsinki
     let startTime = DateTime.Now
     let endTime = startTime.AddHours(1)
-    let attendees: List<Attendee> = [NewAttendee()]
+    let attendees = ResizeArray<Attendee>([NewAttendee()])
     
     // act
     let booking = Booking(id, room, startTime, endTime, attendees)
@@ -33,7 +33,7 @@ let ``Create new booking appends BookingCreated event`` () =
     Assert.Equal(room, booking.MeetingRoom)
     Assert.Equal(startTime, booking.StartTime);
     Assert.Equal(endTime, booking.EndTime);
-    Assert.Equal(attendees, booking.Attendees)
+    Assert.Equal(attendees.ToArray(), booking.Attendees)
     Assert.Single(booking.Events) |> ignore
 
     match booking.Events[0] with
@@ -42,5 +42,38 @@ let ``Create new booking appends BookingCreated event`` () =
             Assert.Equal(room, actualMeetingRoom)
             Assert.Equal(startTime, actualStartTime)
             Assert.Equal(endTime, actualEndTime)
-            Assert.Equal(attendees, actualAttendees)
+            Assert.Equal(attendees.ToArray(), actualAttendees)
         | _ -> Assert.Fail("The event is not a BookingCreated event")
+
+[<Fact>]
+let ``Update time slot appends BookedTimeSlotChanged event`` () =
+    // arrange
+    let initialStartTime = DateTime.Now
+    let initialEndTime = initialStartTime.AddHours(1)
+    let attendees = ResizeArray<Attendee>([NewAttendee()])
+    let booking = Booking(Guid.CreateVersion7(), MeetingRoom.Helsinki, initialStartTime, initialEndTime, attendees)
+
+    // act
+    let newStartTime = initialStartTime.AddHours(1)
+    let newEndTime = newStartTime.AddHours(2)
+    booking.UpdateTimeSlot(newStartTime, newEndTime)
+    
+    // assert
+    Assert.Equal(booking.StartTime, booking.StartTime);
+    Assert.Equal(booking.EndTime, booking.EndTime)
+    Assert.Equal(2, booking.Events.Count) |> ignore
+    match booking.Events[0] with
+        | BookingCreated(actualId, actualMeetingRoom, actualStartTime, actualEndTime, actualAttendees) ->
+            Assert.Equal(booking.Id, actualId)
+            Assert.Equal(booking.MeetingRoom, actualMeetingRoom)
+            Assert.Equal(initialStartTime, actualStartTime)
+            Assert.Equal(initialEndTime, actualEndTime)
+            Assert.Equal(booking.Attendees.ToArray(), actualAttendees)
+        | _ -> Assert.Fail("The first event is not a BookingCreated event")
+    match booking.Events[1] with
+        | BookedTimeSlotChanged(actualStartTime, actualEndTime) ->
+            Assert.Equal(newStartTime, actualStartTime)
+            Assert.Equal(newEndTime, actualEndTime)
+        | _ -> Assert.Fail("The second event is not a BookedTimeSlotChanged event")
+
+    
